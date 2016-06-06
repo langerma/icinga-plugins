b5468a8ef9cedfb43abeeafc1e56d1c209738a7d
Markus Langer <langer.markus@gmail.com>
2016-06-07 00:15:20 +0200

    fileshit

--- check_file_age.py
+++ check_file_age.py
@@ -1,7 +1,16 @@
 import os
 import time
+import argparse
 
-print "last accessed on" + str(time.ctime(os.path.getatime('/home/langerma/.vim/vimrc')))
-print "last modified on" + str(time.ctime(os.path.getmtime('/home/langerma/.vim/vimrc')))
-print "last asdjlaj  on" + str(time.ctime(os.path.getctime('/home/langerma/.vim/vimrc')))
 
+def check(filename):
+    #print "last accessed on" + str(time.ctime(os.path.getatime('/home/langerma/.vim/vimrc')))
+    print "last modified on" + str(time.ctime(os.path.getmtime(filename)))
+    #print "last asdjlaj  on" + str(time.ctime(os.path.getctime('/home/langerma/.vim/vimrc')))
+
+
+if  __name__ == '__main__':
+    parser = argparse.ArgumentParser('Icinga check for fileage')
+    parser.add_argument('--file', required=True, help='filename or path')
+    args = parser.parse_args()
+    check(file=str(args.file))
