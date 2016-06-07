e69f8e08347af544e82c614ef7b07ce829e8108c
Markus Langer <langer.markus@gmail.com>
2016-06-07 09:18:43 +0200

    added relative time

--- check_file_age.py
+++ check_file_age.py
@@ -1,16 +1,41 @@
+#!/usr/bin/python
+
+###############################################################################
+#                             check_file_age.py                               #
+#                       (c) langer.markus@gmail.com                           #
+###############################################################################
+
+import sys
 import os
 import time
+import datetime
 import argparse
 
 
-def check(filename):
-    #print "last accessed on" + str(time.ctime(os.path.getatime('/home/langerma/.vim/vimrc')))
-    print "last modified on" + str(time.ctime(os.path.getmtime(filename)))
-    #print "last asdjlaj  on" + str(time.ctime(os.path.getctime('/home/langerma/.vim/vimrc')))
-
+def check(filename, text, method):
+    current_time = time.time()
+    if method == 'accessed':
+        fileage = os.path.getatime(filename)
+        fileage_relative = int(current_time) - int(fileage)
+        print str(text) + str(time.ctime(fileage)) + " relative time: " + str(datetime.timedelta(seconds=fileage_relative))
+        sys.exit(0)
+    elif method == 'metadata':
+        fileage = os.path.getmtime(filename)
+        fileage_relative = int(current_time) - int(fileage)
+        print str(text) + str(time.ctime(fileage)) + " relative time: " + str(datetime.timedelta(seconds=fileage_relative))
+        sys.exit(0)
+    elif method == 'modified':
+        fileage = os.path.getctime(filename)
+        fileage_relative = int(current_time) - int(fileage)
+        print str(text) + str(time.ctime(fileage)) + " relative time: " + str(datetime.timedelta(seconds=fileage_relative))
+        sys.exit(0)
+    else:
+        print "Error: false method"
 
 if  __name__ == '__main__':
     parser = argparse.ArgumentParser('Icinga check for fileage')
-    parser.add_argument('--file', required=True, help='filename or path')
+    parser.add_argument('--filename', required=True, help='filename or path')
+    parser.add_argument('--text', default='last modified on: ', help='Custom Text (e.g. last modified on:)')
+    parser.add_argument('--method', default='modified', choices=['modified','accessed','metadata'], help='Method what you want to check')
     args = parser.parse_args()
-    check(file=str(args.file))
+    check(filename=args.filename,text=args.text,method=args.method)
