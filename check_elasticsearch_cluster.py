f844bc4a4247a760a48085295d022d2d5867d3ca
Markus Langer <langer.markus@gmail.com>
2016-06-08 16:48:24 +0200

    added metric check

--- check_elasticsearch_cluster.py
+++ check_elasticsearch_cluster.py
@@ -10,42 +10,41 @@
 check_elasticsearch_cluster.py
 Copyright (C) 2016  langer.markus@gmail.com
 
-# searchstring
+# simple searchstring for timerange
 
 {
-    "query": {
-        "match": {
-            "netflow.output_snmp": {
-                "query": 14,
-                "type": "phrase"
-            }
-        }
-        "filter": {
-            "range": {
-                {field-name}: {
-                    "from": {lower-value}
-                    "to": {upper-value}
-                }
-            }
-        }
+    "range" : {
+        "timestamp" : {"gt" : "now-1h"}
     }
 }
+
+# combined searchstring 
 
 {
     "query": {
         "filtered": {
-            "query": {"match_all": {}},
+            "query": {
+                "match_all": {}
+            },
             "filter": {
                 "and": [
-                    {"term": {"netflow.output_snmp": "14"}},
-                    {"term": {"books.genre": "scifi"}}
+                    {
+                        "range" : {
+                            "timestamp" : { 
+                                "gt" : "now-15m"
+                            }
+                        },
+                    },
+                    {
+                        "term": {
+                            "field": "value"
+                        }
+                    }
                 ]
             }
         }
     }
 }
-
-
 
 json.load(urllib.urlopen('http://www.langerma.org/elasticsearch/_search?search_type=count', data='searchstring'))
 
@@ -57,10 +56,10 @@ import sys
 import json
 import urllib
 import argparse
+import pprint
 
-def health(elasticsearch_host, elasticsearch_port):
-    data_url = "http://" + str(elasticsearch_host) + ":" + str(elasticsearch_port) + "/_cluster/health"
-    elasticsearch_cluster = json.load(urllib.urlopen(data_url))
+def health(data_url):
+    elasticsearch_cluster = json.load(urllib.urlopen(str(data_url) + '/_cluster/health'))
     icingaout = "timed out: " + str(elasticsearch_cluster["timed_out"]) + \
         "; nodes: " + str(elasticsearch_cluster["number_of_nodes"]) + \
         "; data nodes: " + str(elasticsearch_cluster["number_of_data_nodes"]) + \
@@ -80,46 +79,31 @@ def health(elasticsearch_host, elasticsearch_port):
         print "Cluster: " + str(elasticsearch_cluster["status"])+ " | " + str(icingaout)
         sys.exit(0)
 
-def field():
-    #json.load(urllib.urlopen(elasticsearch, data=searchstring))
-    pass
+def metric(data_url, field, value, critical, warning, duration):
+    searchstring = '{"sort": {"date": "asc"}}'
 
-    #def main():
-    #    try:
-    #        elasticsearch_check = str(args[1])
-    #        if elasticsearch_check == "health":
-    #            #elasticsearch_host = str(args[2])
-    #            #elasticsearch_port = str(args[3])
-    #            health(str(args[2]),str(args[3]))
-    #    except:
-    #        print "shit happens -- too few parameters"
-    #        sys.exit(5)
+    query_data = json.load(urllib.urlopen(str(data_url) + '/_all/_search' , data=searchstring))
+    pprint.pprint(query_data)
 
 if __name__ == '__main__':
+    # request parameters for script
     parser = argparse.ArgumentParser('Icinga check for elasticsearch')
     parser.add_argument('--host', required=True, help='elasticsearch host')
-    parser.add_argument('--port', type=int, help='port that elasticsearch is running on (eg. 9200)')
+    parser.add_argument('--port', type=int, default=9200, help='port that elasticsearch is running on (eg. 9200)')
     parser.add_argument('--uri', help='Uri for elasticsearch for example /elasticsearch')
     parser.add_argument('--command', default='health', choices=['health','metric'], help='check command')
     parser.add_argument('--field', help='Field you want to check e.g. logins_failed')
+    parser.add_argument('--value', help='Query this value')
     parser.add_argument('--critical', help='Critical threshold, e.g. 1, 100')
     parser.add_argument('--warning', help='Warning threshold, e.g. 1, 20')
-    parser.add_argument('--duration', type=int, default=5, help='Number of minutes of data to aggregate')
+    parser.add_argument('--duration', default=5, help='e.g: 1h, 15m, 32d')
     args = parser.parse_args()
-    if args.command == "health":
-        pass
+    try:
+        data_url = "http://" + str(args.host) + ":" + str(args.port) + "/" + str(args.uri)
+    except:
+        print "something went wrong with the url shit"
+    # logic to call the right functions
+    if args.command=="metric":
+        metric(data_url, args.field, args.value, args.critical, args.warning, args.duration)
     else:
-        health(elasticsearch_string)
-
-    #check(host=args.host,
-    #      metric=args.metric,
-    #      warning_threshold=args.warning,
-    #      critical_threshold=args.critical,
-    #      invert=args.invert,
-    #      function=args.function,
-    #      duration=args.duration,
-    #      user=args.user,
-    #      password=args.password,
-    #      grafana=args.grafana,
-    #      datasourceid=args.datasourceid,
-    #      authkey=args.authkey)
+        health(data_url)
