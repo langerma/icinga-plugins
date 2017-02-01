e99fd9df65cf38c0b72619d3dd5427e6db8378e8
Markus Langer <langer.markus@gmail.com>
2017-02-01 15:04:10 +0100

    beautified code

--- check_file_age.py
+++ check_file_age.py
@@ -9,24 +9,30 @@
 import sys
 import os
 import time
-import datetime
 import argparse
 
 # icinga returncode vars
-EXIT_OK         = 0
-EXIT_WARNING    = 1
-EXIT_CRITICAL   = 2
-EXIT_UNKNOWN    = 3
+EXIT_OK = 0
+EXIT_WARNING = 1
+EXIT_CRITICAL = 2
+EXIT_UNKNOWN = 3
+
 
 def thresholds(filename, timedelta, warning, critical):
     warning = (warning * 3600)
     critical = (critical * 3600)
-    message = '- %s is %s old |current_age=%s;warning=%s;critical=%s' %(filename, timedelta, timedelta, warning, critical)
+    message = '- %s is %s old |current_age=%s;warning=%s;critical=%s' % (
+            filename,
+            timedelta,
+            timedelta,
+            warning,
+            critical)
     if timedelta > critical:
         critical_exit(message)
     if timedelta > warning:
         warning_exit(message)
     ok_exit(message)
+
 
 def check(filename, method, warning, critical):
     current_time = time.time()
@@ -40,33 +46,55 @@ def check(filename, method, warning, critical):
         unknown_exit("Error: false method")
 
     timedelta = int(current_time) - int(fileage)
-    thresholds(filename=filename, timedelta=timedelta, warning=warning, critical=critical)
+    thresholds(
+            filename=filename,
+            timedelta=timedelta,
+            warning=warning,
+            critical=critical)
+
 
 # icinga returncode functions
 def critical_exit(message):
     print 'CRITICAL %s' % message
     sys.exit(EXIT_CRITICAL)
 
+
 def warning_exit(message):
     print 'WARNING %s' % message
     sys.exit(EXIT_WARNING)
+
 
 def ok_exit(message):
     print 'OK %s' % message
     sys.exit(EXIT_OK)
 
+
 def unknown_exit(message):
     print 'UNKNOWN %s' % message
     sys.exit(EXIT_UNKNOWN)
 
-if  __name__ == '__main__':
+
+if __name__ == '__main__':
     parser = argparse.ArgumentParser('Icinga check for fileage')
     parser.add_argument('--filename', required=True, help='filename or path')
-    parser.add_argument('--method', default='modified', choices=['modified','accessed','metadata'], help='Method what you want to check')
-    parser.add_argument('--warning', default=24, type=int, help='Warning threshold in hours')
-    parser.add_argument('--critical', default=48, type=int, help='Critical threshold in hours')
+    parser.add_argument(
+            '--method',
+            default='modified',
+            choices=['modified', 'accessed', 'metadata'],
+            help='Method what you want to check')
+    parser.add_argument(
+            '--warning',
+            default=24,
+            type=int,
+            help='Warning threshold in hours')
+    parser.add_argument(
+            '--critical',
+            default=48,
+            type=int,
+            help='Critical threshold in hours')
     args = parser.parse_args()
-    check(filename=args.filename,
+    check(
+            filename=args.filename,
             method=args.method,
             warning=args.warning,
             critical=args.critical)
